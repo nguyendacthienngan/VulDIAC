b147e748e87fa7a0ba0773caf92c98ea3bbcae77
foresta-y <shuailin.yang@hotmail.com>
2025-08-15 02:32:48 +0800

    a

--- scripts/normalization.py
+++ scripts/normalization.py
@@ -76,56 +76,5 @@ if __name__ == '__main__':
     main()
     print("Normalization Done!")
 
-
-# import os
-# import re
-# import argparse
-# import glob
-# from clean_gadget import clean_gadget
-# from tqdm import tqdm
-
-# def parse_options():
-#     parser = argparse.ArgumentParser(description='Normalization.')
-#     parser.add_argument('-i', '--input', help='The dir path of input dataset', type=str, required=True)
-#     args = parser.parse_args()
-#     return args
-
-# def normalize(path):
-#     cpp_files = glob.glob(path + "/*.c")
-#     for cpp_file in tqdm(cpp_files):
-#         try:
-#             pro_one_file(cpp_file)
-#         except:
-#             # 删除该文件
-#             os.remove(cpp_file)
-                
-
-# def pro_one_file(filepath):
-#     with open(filepath, "r") as file:
-#         code = file.read()
-
-#     file.close()
-#     code = re.sub('(?<!:)\\/\\/.*|\\/\\*(\\s|.)*?\\*\\/', "", code)
-#     # print(code)
-#     with open(filepath, "w") as file:
-#         file.write(code.strip())
-#     file.close()
-
-#     with open(filepath, "r") as file:
-#         org_code = file.readlines()
-#         nor_code = clean_gadget(org_code)
-#     file.close()
-#     with open(filepath, "w") as file:
-#         file.writelines(nor_code)
-#     file.close()
-
-# def main():
-#     args = parse_options()
-#     normalize(args.input)
-    
-# if __name__ == '__main__':
-#     print("Start Normalization...")
-#     main()
-#     print("Normalization Done!")
     
     
